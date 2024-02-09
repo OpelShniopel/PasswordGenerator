75658ce0a6e084f71b646fa3bf217e5872fa94ee
OpelShniopel <112033023+OpelShniopel@users.noreply.github.com>
2024-02-09 23:54:08 +0200

    Initial commit

---
+++ PasswordGenerator/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace PasswordGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            checkBoxUpperCase = new CheckBox();
+            checkBoxLowerCase = new CheckBox();
+            checkBoxNumbers = new CheckBox();
+            checkBoxSpecialChar = new CheckBox();
+            textBoxGeneratedPassword = new TextBox();
+            trackBarPasswordLength = new TrackBar();
+            buttonGeneratePassword = new Button();
+            checkBoxAvoidAmbiguousCharacters = new CheckBox();
+            numericUpDownMinNumbers = new NumericUpDown();
+            numericUpDownMinSpecial = new NumericUpDown();
+            labelMinNumbers = new Label();
+            labelMinSpecial = new Label();
+            labelPasswordLength = new Label();
+            numericUpDownPasswordLength = new NumericUpDown();
+            buttonCopy = new Button();
+            checkBoxExcludeDuplicateCharacters = new CheckBox();
+            checkBoxAllowOtherSymbols = new CheckBox();
+            ((System.ComponentModel.ISupportInitialize)trackBarPasswordLength).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownMinNumbers).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownMinSpecial).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownPasswordLength).BeginInit();
+            SuspendLayout();
+            // 
+            // checkBoxUpperCase
+            // 
+            checkBoxUpperCase.AutoSize = true;
+            checkBoxUpperCase.Checked = true;
+            checkBoxUpperCase.CheckState = CheckState.Checked;
+            checkBoxUpperCase.Location = new Point(32, 95);
+            checkBoxUpperCase.Name = "checkBoxUpperCase";
+            checkBoxUpperCase.Size = new Size(46, 19);
+            checkBoxUpperCase.TabIndex = 0;
+            checkBoxUpperCase.Text = "A-Z";
+            checkBoxUpperCase.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxLowerCase
+            // 
+            checkBoxLowerCase.AutoSize = true;
+            checkBoxLowerCase.Checked = true;
+            checkBoxLowerCase.CheckState = CheckState.Checked;
+            checkBoxLowerCase.Location = new Point(32, 120);
+            checkBoxLowerCase.Name = "checkBoxLowerCase";
+            checkBoxLowerCase.Size = new Size(42, 19);
+            checkBoxLowerCase.TabIndex = 1;
+            checkBoxLowerCase.Text = "a-z";
+            checkBoxLowerCase.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxNumbers
+            // 
+            checkBoxNumbers.AutoSize = true;
+            checkBoxNumbers.Checked = true;
+            checkBoxNumbers.CheckState = CheckState.Checked;
+            checkBoxNumbers.Location = new Point(32, 145);
+            checkBoxNumbers.Name = "checkBoxNumbers";
+            checkBoxNumbers.Size = new Size(43, 19);
+            checkBoxNumbers.TabIndex = 2;
+            checkBoxNumbers.Text = "0-9";
+            checkBoxNumbers.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxSpecialChar
+            // 
+            checkBoxSpecialChar.AutoSize = true;
+            checkBoxSpecialChar.Checked = true;
+            checkBoxSpecialChar.CheckState = CheckState.Checked;
+            checkBoxSpecialChar.Location = new Point(32, 170);
+            checkBoxSpecialChar.Name = "checkBoxSpecialChar";
+            checkBoxSpecialChar.Size = new Size(76, 19);
+            checkBoxSpecialChar.TabIndex = 3;
+            checkBoxSpecialChar.Text = "!@#$%^*";
+            checkBoxSpecialChar.UseVisualStyleBackColor = true;
+            // 
+            // textBoxGeneratedPassword
+            // 
+            textBoxGeneratedPassword.Location = new Point(32, 12);
+            textBoxGeneratedPassword.Name = "textBoxGeneratedPassword";
+            textBoxGeneratedPassword.Size = new Size(209, 23);
+            textBoxGeneratedPassword.TabIndex = 4;
+            textBoxGeneratedPassword.TextAlign = HorizontalAlignment.Center;
+            // 
+            // trackBarPasswordLength
+            // 
+            trackBarPasswordLength.Location = new Point(147, 41);
+            trackBarPasswordLength.Maximum = 128;
+            trackBarPasswordLength.Minimum = 5;
+            trackBarPasswordLength.Name = "trackBarPasswordLength";
+            trackBarPasswordLength.Size = new Size(256, 45);
+            trackBarPasswordLength.TabIndex = 5;
+            trackBarPasswordLength.Value = 16;
+            trackBarPasswordLength.Scroll += trackBarPasswordLength_Scroll;
+            // 
+            // buttonGeneratePassword
+            // 
+            buttonGeneratePassword.Location = new Point(328, 12);
+            buttonGeneratePassword.Name = "buttonGeneratePassword";
+            buttonGeneratePassword.Size = new Size(75, 23);
+            buttonGeneratePassword.TabIndex = 6;
+            buttonGeneratePassword.Text = "Generate";
+            buttonGeneratePassword.UseVisualStyleBackColor = true;
+            buttonGeneratePassword.Click += buttonGeneratePassword_Click;
+            // 
+            // checkBoxAvoidAmbiguousCharacters
+            // 
+            checkBoxAvoidAmbiguousCharacters.AutoSize = true;
+            checkBoxAvoidAmbiguousCharacters.Checked = true;
+            checkBoxAvoidAmbiguousCharacters.CheckState = CheckState.Checked;
+            checkBoxAvoidAmbiguousCharacters.Location = new Point(32, 268);
+            checkBoxAvoidAmbiguousCharacters.Name = "checkBoxAvoidAmbiguousCharacters";
+            checkBoxAvoidAmbiguousCharacters.Size = new Size(177, 19);
+            checkBoxAvoidAmbiguousCharacters.TabIndex = 7;
+            checkBoxAvoidAmbiguousCharacters.Text = "Avoid ambiguous characters";
+            checkBoxAvoidAmbiguousCharacters.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDownMinNumbers
+            // 
+            numericUpDownMinNumbers.Location = new Point(147, 210);
+            numericUpDownMinNumbers.Maximum = new decimal(new int[] { 9, 0, 0, 0 });
+            numericUpDownMinNumbers.Name = "numericUpDownMinNumbers";
+            numericUpDownMinNumbers.Size = new Size(28, 23);
+            numericUpDownMinNumbers.TabIndex = 8;
+            numericUpDownMinNumbers.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDownMinNumbers.ValueChanged += numericUpDownMinNumbers_ValueChanged;
+            // 
+            // numericUpDownMinSpecial
+            // 
+            numericUpDownMinSpecial.Location = new Point(147, 239);
+            numericUpDownMinSpecial.Maximum = new decimal(new int[] { 9, 0, 0, 0 });
+            numericUpDownMinSpecial.Name = "numericUpDownMinSpecial";
+            numericUpDownMinSpecial.Size = new Size(29, 23);
+            numericUpDownMinSpecial.TabIndex = 9;
+            numericUpDownMinSpecial.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDownMinSpecial.ValueChanged += numericUpDownMinSpecial_ValueChanged;
+            // 
+            // labelMinNumbers
+            // 
+            labelMinNumbers.AutoSize = true;
+            labelMinNumbers.Location = new Point(30, 212);
+            labelMinNumbers.Name = "labelMinNumbers";
+            labelMinNumbers.Size = new Size(110, 15);
+            labelMinNumbers.TabIndex = 10;
+            labelMinNumbers.Text = "Minimum numbers";
+            // 
+            // labelMinSpecial
+            // 
+            labelMinSpecial.AutoSize = true;
+            labelMinSpecial.Location = new Point(30, 241);
+            labelMinSpecial.Name = "labelMinSpecial";
+            labelMinSpecial.Size = new Size(99, 15);
+            labelMinSpecial.TabIndex = 11;
+            labelMinSpecial.Text = "Minimum special";
+            // 
+            // labelPasswordLength
+            // 
+            labelPasswordLength.AutoSize = true;
+            labelPasswordLength.Location = new Point(30, 41);
+            labelPasswordLength.Name = "labelPasswordLength";
+            labelPasswordLength.Size = new Size(44, 15);
+            labelPasswordLength.TabIndex = 12;
+            labelPasswordLength.Text = "Length";
+            // 
+            // numericUpDownPasswordLength
+            // 
+            numericUpDownPasswordLength.Location = new Point(92, 41);
+            numericUpDownPasswordLength.Maximum = new decimal(new int[] { 128, 0, 0, 0 });
+            numericUpDownPasswordLength.Minimum = new decimal(new int[] { 5, 0, 0, 0 });
+            numericUpDownPasswordLength.Name = "numericUpDownPasswordLength";
+            numericUpDownPasswordLength.Size = new Size(38, 23);
+            numericUpDownPasswordLength.TabIndex = 13;
+            numericUpDownPasswordLength.Value = new decimal(new int[] { 16, 0, 0, 0 });
+            numericUpDownPasswordLength.ValueChanged += numericUpDownPasswordLength_ValueChanged;
+            // 
+            // buttonCopy
+            // 
+            buttonCopy.Location = new Point(247, 12);
+            buttonCopy.Name = "buttonCopy";
+            buttonCopy.Size = new Size(75, 23);
+            buttonCopy.TabIndex = 14;
+            buttonCopy.Text = "Copy";
+            buttonCopy.UseVisualStyleBackColor = true;
+            buttonCopy.Click += buttonCopy_Click;
+            // 
+            // checkBoxExcludeDuplicateCharacters
+            // 
+            checkBoxExcludeDuplicateCharacters.AutoSize = true;
+            checkBoxExcludeDuplicateCharacters.Location = new Point(158, 92);
+            checkBoxExcludeDuplicateCharacters.Name = "checkBoxExcludeDuplicateCharacters";
+            checkBoxExcludeDuplicateCharacters.Size = new Size(176, 19);
+            checkBoxExcludeDuplicateCharacters.TabIndex = 15;
+            checkBoxExcludeDuplicateCharacters.Text = "Exclude duplicate characters";
+            checkBoxExcludeDuplicateCharacters.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxAllowOtherSymbols
+            // 
+            checkBoxAllowOtherSymbols.AutoSize = true;
+            checkBoxAllowOtherSymbols.Location = new Point(158, 117);
+            checkBoxAllowOtherSymbols.Name = "checkBoxAllowOtherSymbols";
+            checkBoxAllowOtherSymbols.Size = new Size(191, 19);
+            checkBoxAllowOtherSymbols.TabIndex = 16;
+            checkBoxAllowOtherSymbols.Text = "Allow other symbols (~`[];?,()+)";
+            checkBoxAllowOtherSymbols.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(430, 304);
+            Controls.Add(checkBoxAllowOtherSymbols);
+            Controls.Add(checkBoxExcludeDuplicateCharacters);
+            Controls.Add(buttonCopy);
+            Controls.Add(numericUpDownPasswordLength);
+            Controls.Add(labelPasswordLength);
+            Controls.Add(labelMinSpecial);
+            Controls.Add(labelMinNumbers);
+            Controls.Add(numericUpDownMinSpecial);
+            Controls.Add(numericUpDownMinNumbers);
+            Controls.Add(checkBoxAvoidAmbiguousCharacters);
+            Controls.Add(buttonGeneratePassword);
+            Controls.Add(trackBarPasswordLength);
+            Controls.Add(textBoxGeneratedPassword);
+            Controls.Add(checkBoxSpecialChar);
+            Controls.Add(checkBoxNumbers);
+            Controls.Add(checkBoxLowerCase);
+            Controls.Add(checkBoxUpperCase);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "PassGen V3";
+            ((System.ComponentModel.ISupportInitialize)trackBarPasswordLength).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownMinNumbers).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownMinSpecial).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownPasswordLength).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private CheckBox checkBoxUpperCase;
+        private CheckBox checkBoxLowerCase;
+        private CheckBox checkBoxNumbers;
+        private CheckBox checkBoxSpecialChar;
+        private TextBox textBoxGeneratedPassword;
+        private TrackBar trackBarPasswordLength;
+        private Button buttonGeneratePassword;
+        private CheckBox checkBoxAvoidAmbiguousCharacters;
+        private NumericUpDown numericUpDownMinNumbers;
+        private NumericUpDown numericUpDownMinSpecial;
+        private Label labelMinNumbers;
+        private Label labelMinSpecial;
+        private Label labelPasswordLength;
+        private NumericUpDown numericUpDownPasswordLength;
+        private Button buttonCopy;
+        private CheckBox checkBoxExcludeDuplicateCharacters;
+        private CheckBox checkBoxAllowOtherSymbols;
+    }
+}
---
+++ PasswordGenerator/Form1.cs
@@ -0,0 +1,245 @@
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PasswordGenerator;
+
+public partial class Form1 : Form
+{
+    private const string LowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    private const string UppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private const string NumberChars = "0123456789";
+    private const string SpecialChars = "!@#$%^&*";
+    private const string OtherSpecialChars = "~`[];?,()+";
+
+    private readonly RandomNumberGenerator _randomGenerator = RandomNumberGenerator.Create();
+
+    public Form1()
+    {
+        InitializeComponent();
+
+        GeneratePassword();
+    }
+
+    private void GeneratePassword()
+    {
+        StringBuilder passwordBuilder = new();
+
+        string characterPool = BuildCharacterPool();
+        AddRequiredCharacters(passwordBuilder);
+        FillPassword(passwordBuilder, characterPool);
+
+        if (checkBoxExcludeDuplicateCharacters.Checked)
+            ExcludeDuplicateCharacters(passwordBuilder);
+
+        ShufflePassword(passwordBuilder);
+
+        textBoxGeneratedPassword.Text = passwordBuilder.ToString();
+    }
+
+    private char GetRandomCharacter(string characters)
+    {
+        if (characters.Length == 0)
+            throw new ArgumentException(@"Characters should not be empty", nameof(characters));
+
+        // Calculate the maximum value that is evenly divisible by the character pool length
+        int maxVal = (int.MaxValue / characters.Length) * characters.Length;
+        byte[] randomNumber = new byte[4];
+        int randomValue;
+
+        do
+        {
+            _randomGenerator.GetBytes(randomNumber);
+            randomValue = BitConverter.ToInt32(randomNumber, 0) & int.MaxValue; // Ensure non-negative
+        } while (randomValue >= maxVal); // Reject values that would introduce bias
+
+        return characters[randomValue % characters.Length];
+    }
+
+    private int GetRandomNumber(int maxExclusive)
+    {
+        if (maxExclusive <= 0)
+            throw new ArgumentOutOfRangeException(nameof(maxExclusive), @"maxExclusive must be positive.");
+
+        int maxVal = (int.MaxValue / maxExclusive) * maxExclusive;
+        byte[] randomNumber = new byte[4];
+        int value;
+
+        do
+        {
+            _randomGenerator.GetBytes(randomNumber);
+            value = BitConverter.ToInt32(randomNumber, 0) & int.MaxValue; // Ensure non-negative
+        } while (value >= maxVal); // Reject values that would introduce bias
+
+        return value % maxExclusive;
+    }
+
+    private static string RemoveAmbiguousCharacters(string characterPool)
+    {
+        const string ambiguousCharacters = "lIO01";
+        StringBuilder newCharacterPool = new(characterPool.Length);
+
+        foreach (char c in characterPool)
+        {
+            if (!ambiguousCharacters.Contains(c))
+                newCharacterPool.Append(c);
+        }
+
+        return newCharacterPool.ToString();
+    }
+    
+    private void AppendCharacters(CheckBox checkBox, string characters, StringBuilder builder, int numCharacters = 1)
+    {
+        if (!checkBox.Checked)
+            return;
+
+        string characterSet = checkBoxAvoidAmbiguousCharacters.Checked
+            ? RemoveAmbiguousCharacters(characters)
+            : characters;
+
+        for (int i = 0; i < numCharacters; i++)
+            builder.Append(GetRandomCharacter(characterSet));
+    }
+    
+    private void AddRequiredCharacters(StringBuilder passwordBuilder)
+    {
+        AppendCharacters(checkBoxLowerCase, LowercaseChars, passwordBuilder);
+        AppendCharacters(checkBoxUpperCase, UppercaseChars, passwordBuilder);
+        AppendCharacters(checkBoxNumbers, NumberChars, passwordBuilder, (int)numericUpDownMinNumbers.Value);
+        AppendCharacters(checkBoxSpecialChar, SpecialChars, passwordBuilder, (int)numericUpDownMinSpecial.Value);
+        AppendCharacters(checkBoxAllowOtherSymbols, OtherSpecialChars, passwordBuilder);
+    }
+    
+    private string BuildCharacterPool()
+    {
+        StringBuilder characterPoolBuilder = new();
+
+        // Append characters based on checkbox selections
+        if (checkBoxUpperCase.Checked)
+            characterPoolBuilder.Append(UppercaseChars);
+        if (checkBoxLowerCase.Checked)
+            characterPoolBuilder.Append(LowercaseChars);
+        if (checkBoxNumbers.Checked)
+            characterPoolBuilder.Append(NumberChars);
+        if (checkBoxSpecialChar.Checked)
+            characterPoolBuilder.Append(SpecialChars);
+        if (checkBoxAllowOtherSymbols.Checked)
+            characterPoolBuilder.Append(OtherSpecialChars);
+
+        // Default to lowercase if no option is selected
+        if (characterPoolBuilder.Length == 0)
+        {
+            checkBoxLowerCase.Checked = true;
+            characterPoolBuilder.Append(LowercaseChars);
+        }
+
+        string characterPool = characterPoolBuilder.ToString();
+
+        // Remove ambiguous characters if the option is selected
+        if (checkBoxAvoidAmbiguousCharacters.Checked)
+            characterPool = RemoveAmbiguousCharacters(characterPool);
+
+        return characterPool;
+    }
+    
+    private void FillPassword(StringBuilder passwordBuilder, string characterPool)
+    {
+        for (int i = passwordBuilder.Length; i < trackBarPasswordLength.Value; i++)
+        {
+            char nextChar = GetRandomCharacter(characterPool);
+            passwordBuilder.Append(nextChar);
+        }
+    }
+
+    // Fisher Yates shuffle
+    private void ShufflePassword(StringBuilder passwordBuilder)
+    {
+        char[] passwordArray = passwordBuilder.ToString().ToCharArray();
+        int n = passwordArray.Length;
+
+        while (n > 1)
+        {
+            n--;
+            int k = GetRandomNumber(n + 1);
+            (passwordArray[k], passwordArray[n]) = (passwordArray[n], passwordArray[k]);
+        }
+
+        passwordBuilder.Clear();
+        passwordBuilder.Append(passwordArray);
+    }
+    
+    private void ExcludeDuplicateCharacters(StringBuilder passwordBuilder)
+    {
+        HashSet<char> uniqueCharacters = new();
+        HashSet<char> characterPool = new(BuildCharacterPool().ToHashSet());
+        StringBuilder newPassword = new(passwordBuilder.Length);
+
+        // First pass: Add unique characters to newPassword and update characterPool
+        foreach (char c in passwordBuilder.ToString())
+        {
+            if (uniqueCharacters.Add(c))
+            {
+                newPassword.Append(c);
+                characterPool.Remove(c);
+            }
+        }
+
+        // Second pass: Fill the password back to the required length with random characters
+        List<char> remainingCharacterPool = characterPool.ToList();
+
+        while (newPassword.Length < trackBarPasswordLength.Value && remainingCharacterPool.Count > 0)
+        {
+            int index = GetRandomNumber(remainingCharacterPool.Count);
+            newPassword.Append(remainingCharacterPool[index]);
+            remainingCharacterPool.RemoveAt(index);
+        }
+
+        // Replace the original passwordBuilder content
+        passwordBuilder.Clear();
+        passwordBuilder.Append(newPassword);
+    }
+
+    private void ValidateAndUpdatePasswordLength()
+    {
+        int totalMinimumRequiredChars = (int)(numericUpDownMinNumbers.Value + numericUpDownMinSpecial.Value);
+
+        if (trackBarPasswordLength.Value >= totalMinimumRequiredChars)
+            return;
+
+        trackBarPasswordLength.Value = totalMinimumRequiredChars;
+        numericUpDownPasswordLength.Value = totalMinimumRequiredChars;
+    }
+
+    private void buttonGeneratePassword_Click(object sender, EventArgs e)
+    {
+        GeneratePassword();
+    }
+
+    private void buttonCopy_Click(object sender, EventArgs e)
+    {
+        Clipboard.SetText(textBoxGeneratedPassword.Text);
+    }
+
+    private void trackBarPasswordLength_Scroll(object sender, EventArgs e)
+    {
+        numericUpDownPasswordLength.Value = trackBarPasswordLength.Value;
+        GeneratePassword();
+    }
+
+    private void numericUpDownMinNumbers_ValueChanged(object sender, EventArgs e)
+    {
+        ValidateAndUpdatePasswordLength();
+        GeneratePassword();
+    }
+
+    private void numericUpDownMinSpecial_ValueChanged(object sender, EventArgs e)
+    {
+        ValidateAndUpdatePasswordLength();
+        GeneratePassword();
+    }
+
+    private void numericUpDownPasswordLength_ValueChanged(object sender, EventArgs e)
+    {
+        trackBarPasswordLength.Value = (int)numericUpDownPasswordLength.Value;
+        GeneratePassword();
+    }
+}
